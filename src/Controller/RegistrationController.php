c1c554b7804b261a913816379300b45e0dad7497
cmruizg777 <cmruizg@utn.edu.ec>
2021-05-05 11:40:20 -0500

    email verifier

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -102,8 +102,8 @@ class RegistrationController extends AbstractController
         }
 
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
-        $this->addFlash('success', 'Your email address has been verified.');
+        $this->addFlash('verify_success', 'Your email address has been verified.');
 
-        return $this->redirectToRoute('app_register');
+        return $this->redirectToRoute('app_login');
     }
 }
